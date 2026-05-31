0176b9e1a011f1bb443de1be4761e561d0d8c6ff
Baigars <aigarsinserts@gmail.com>
2026-05-31 18:17:27 +0300

    Set up PyQt6 application with main window

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
-# src/main.py
+import sys
+from PyQt6.QtWidgets import QApplication
+from ui import MainWindow
+
+def main():
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
-    print("denford-virpa main")
+    main()
